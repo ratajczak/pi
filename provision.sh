482783b36d4cbc78b995e8ffb70572cc280f8b78
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2014-04-17 22:05:08 +0100

    Avoid using tilde chracter in provision.sh

--- provision.sh
+++ provision.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 set -e
 
-puppet_base='~/puppet'
-to_provision='~/puppet/manifests/pi.pp'
+puppet_base='/home/pi/puppet'
 
 #sudo apt-get update
 #sudo apt-get install puppet-common
 #sudo apt-get install git
 
-git clone http://github.com/ratajczak/pi.git ~/puppet
-mkdir ${puppet_base}/modules
-puppet module install adrien-network --modulepath=${puppet_base}/modules --version=0.4.1
+git clone http://github.com/ratajczak/pi.git $puppet_base
+mkdir $puppet_base/modules
+puppet module install adrien-network --modulepath=$puppet_base/modules --version=0.4.1
 
 sudo /usr/bin/puppet apply --verbose \
   --manifestdir=${puppet_base}/manifests \
   --modulepath=${puppet_base}/modules \
-  ${to_provision}
+  ${puppet_base}/manifests/pi.pp
