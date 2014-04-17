01451d40b4311bc0420826c6d4af4aa669eff8fa
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2014-04-17 16:01:29 +0000

    Initial commit

---
+++ provision.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb;
+sudo dpkg -i puppetlabs-release-precise.deb;
+sudo apt-get update;
+sudo apt-get install puppet;
+sudo apt-get install git;
+
+git clone http://github.com/ratajczak/pi.git ~/puppet 
+puppet module install adrien-network;
+
+puppet_base='./'
+to_provision='./manifests/pi.pp'
+
+sudo /usr/bin/puppet apply --verbose \
+  --manifestdir=${puppet_base}/manifests \
+  --modulepath=${puppet_base}/modules \
+  ${to_provision}
