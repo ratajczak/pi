763465777753ef8e22d7349c1d6845cdffe5396a
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2014-04-17 21:05:48 +0100

    Remove redundant semicolons

--- provision.sh
+++ provision.sh
@@ -4,13 +4,13 @@ set -e
 puppet_base='./'
 to_provision='./manifests/pi.pp'
 
-sudo apt-get update;
-sudo apt-get install puppet-common;
-sudo apt-get install git;
+sudo apt-get update
+sudo apt-get install puppet-common
+sudo apt-get install git
 
-git clone http://github.com/ratajczak/pi.git ~/puppet 
-mkdir ${puppet_base}/modules;
-puppet module install adrien-network --modulepath=${puppet_base}/modules --version=0.4.1;
+git clone http://github.com/ratajczak/pi.git ~/puppet
+mkdir ${puppet_base}/modules
+puppet module install adrien-network --modulepath=${puppet_base}/modules --version=0.4.1
 
 sudo /usr/bin/puppet apply --verbose \
   --manifestdir=${puppet_base}/manifests \
