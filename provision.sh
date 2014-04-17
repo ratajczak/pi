dd46dd0fb723e40c94bc1669968cb5f2263d8b55
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2014-04-17 17:12:39 +0100

    Use puppet for Debian Wheezy

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb;
+wget https://apt.puppetlabs.com/puppetlabs-release-wheezy.deb;
 sudo dpkg -i puppetlabs-release-precise.deb;
 sudo apt-get update;
 sudo apt-get install puppet;
