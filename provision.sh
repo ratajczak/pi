6ebae21765bf24886925c94b461bb85824991391
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2014-04-17 21:23:44 +0100

    Place puppet files in current user home directory

--- provision.sh
+++ provision.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-puppet_base='./puppet/'
-to_provision='./puppet/manifests/pi.pp'
+puppet_base='~/puppet/'
+to_provision='~/puppet/manifests/pi.pp'
 
 #sudo apt-get update
 sudo apt-get install puppet-common
 sudo apt-get install git
 
-git clone http://github.com/ratajczak/pi.git ~/puppet
+git clone http://github.com/ratajczak/pi.git ${puppet_base}
 mkdir ${puppet_base}/modules
 puppet module install adrien-network --modulepath=${puppet_base}/modules --version=0.4.1
 
