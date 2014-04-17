cffb9473bbeb691db8a76c7d0f21924a9dd7d9bd
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2014-04-17 21:04:21 +0100

    Initialise variables in the beginning of the script

--- provision.sh
+++ provision.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+puppet_base='./'
+to_provision='./manifests/pi.pp'
+
 sudo apt-get update;
 sudo apt-get install puppet-common;
 sudo apt-get install git;
@@ -8,9 +11,6 @@ sudo apt-get install git;
 git clone http://github.com/ratajczak/pi.git ~/puppet 
 mkdir ${puppet_base}/modules;
 puppet module install adrien-network --modulepath=${puppet_base}/modules --version=0.4.1;
-
-puppet_base='./'
-to_provision='./manifests/pi.pp'
 
 sudo /usr/bin/puppet apply --verbose \
   --manifestdir=${puppet_base}/manifests \
