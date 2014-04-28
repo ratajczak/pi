a01244761418873ce6428258910d3e60fe3f2328
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2014-04-28 09:12:34 +0000

    Testing network module

--- manifests/pi.pp
+++ manifests/pi.pp
@@ -1,3 +1,11 @@
-package { "vim":
+node raspberrypi {
+  package { "vim":
     ensure => "latest"
+  }
+  network_config { 'lo':
+    ensure => 'present',
+    family => 'inet',
+    method => 'loopback',
+    onboot => 'true',
+  }
 }
