a1e8af44783fb22d18dcfabd35fc0f344938c6e4
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2014-05-03 11:41:21 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -3,16 +3,21 @@ set -e
 
 PUPPET_DIR='/home/pi/puppet'
 
+echo "Installing dependencies"
 sudo apt-get update
-sudo apt-get install git
-sudo apt-get install puppet-common
-sudo apt-get install ruby-dev
+sudo apt-get install -y git
+sudo apt-get install -y puppet-common
+sudo apt-get install -y ruby-dev
+echo "Installing librarian-puppet"
 sudo gem install librarian-puppet
 
+echo "Clonning puppet files"
 git clone http://github.com/ratajczak/pi.git ${PUPPET_DIR}
 
+echo "Istalling puppet modules"
 cd "${PUPPET_DIR}" && librarian-puppet install --clean
 
+echo "Running pi.pp"
 sudo /usr/bin/puppet apply --verbose \
   --manifestdir=${PUPPET_DIR}/manifests \
   --modulepath=${PUPPET_DIR}/modules \
